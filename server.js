cfc5fdd47c27b697fc9195af33f369a992c4e24b
shan5742 <asamshan456@gmail.com>
2020-11-13 07:50:11 +0000

    trying to fix production error

--- server.js
+++ server.js
@@ -9,23 +9,23 @@ if (process.env.NODE_ENV === "production") {
   // Express to serve production assets
   app.use(express.static(path.join(__dirname, "client", "build")));
 
+  app.get("/weather", async (req, res) => {
+    let response;
+    try {
+      response = await axios.get(
+        "https://www.metaweather.com/api/location/44544"
+      );
+    } catch (e) {
+      res.status(404).send();
+    }
+    res.status(200).send(response.data);
+  });
+
   // Express serve up index.html file if it doesn't recognize route
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
-
-app.get("/weather", async (req, res) => {
-  let response;
-  try {
-    response = await axios.get(
-      "https://www.metaweather.com/api/location/44544"
-    );
-  } catch (e) {
-    res.status(404).send();
-  }
-  res.status(200).send(response.data);
-});
 
 const port = process.env.PORT || 5000;
 
