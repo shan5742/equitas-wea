6c3202d0c6c7f7473c4d9af1bc395539bc23aff3
shan5742 <asamshan456@gmail.com>
2020-11-13 07:57:22 +0000

    fixing production error

--- server.js
+++ server.js
@@ -5,21 +5,21 @@ const app = express();
 const axios = require("axios");
 app.use(cors());
 
+app.get("/weather", async (req, res) => {
+  let response;
+  try {
+    response = await axios.get(
+      "https://www.metaweather.com/api/location/44544"
+    );
+  } catch (e) {
+    res.status(404).send();
+  }
+  res.status(200).send(response.data);
+});
+
 if (process.env.NODE_ENV === "production") {
   // Express to serve production assets
   app.use(express.static(path.join(__dirname, "client", "build")));
-
-  app.get("/weather", async (req, res) => {
-    let response;
-    try {
-      response = await axios.get(
-        "https://www.metaweather.com/api/location/44544"
-      );
-    } catch (e) {
-      res.status(404).send();
-    }
-    res.status(200).send(response.data);
-  });
 
   // Express serve up index.html file if it doesn't recognize route
   app.get("*", (req, res) => {
