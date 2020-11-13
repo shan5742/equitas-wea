f0661ef59885031e9618d78bc0992120a7be5ef9
shan5742 <asamshan456@gmail.com>
2020-11-13 00:26:37 +0000

    adding cors to frontend code to fix production error

--- server.js
+++ server.js
@@ -7,11 +7,10 @@ app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
   // Express to serve production assets
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "client/build")));
   // Express serve up index.html file if it doesn't recognize route
-  const path = require("path");
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
 
